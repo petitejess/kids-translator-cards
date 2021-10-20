564a1b7759d1aaca2dae36c63323a23aacb62b35
Ana Lazarevska <aplazarevska@gmail.com>
2021-10-20 11:25:03 +1100

    add useeffect for api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -7,6 +7,21 @@ import HomePage from './pages/HomePage';
 import ResultPage from './pages/ResultPage';
 
 const App = () => {
+  useEffect(() => {
+    fetch("https://country-facts.p.rapidapi.com/all", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "country-facts.p.rapidapi.com",
+        "x-rapidapi-key": "fb95cae4c5mshcc1e2ab58d6091dp189153jsne2344d05a8f7"
+      }
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, []);
   return (
     <>
       <Header />
@@ -20,3 +35,5 @@ const App = () => {
 }
 
 export default App
+
+
---
+++ src/utils/utils.js
@@ -0,0 +1,6 @@
+// 1 create getCoutryFlagData 
+// 2 export and import it inot app.js
+// 3 put into useEffect
+// 4 see if it works in console.log
+
+// do the same for each api
