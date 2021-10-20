409311d57a5ab6f1562b668c12cefd6de027323f
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-10-20 12:17:19 +1100

    Add scaffold Home Page hallfway

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,46 @@
+import React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+
 const HomePage = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div>
-      Home Page
+      <h1>Kids' Translator Cards</h1>
+      <div>
+        <div>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        </div>
+        <div>
+          {/* Flag component */}
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+      </div>
+      
+
     </div>
   )
 }
