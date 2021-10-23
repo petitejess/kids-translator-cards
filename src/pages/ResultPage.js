7ee3280650b10ee9fcbaa5ab1436c9e58c5dbc92
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-10-23 19:55:21 +1100

    Move the image fetch to Header component, fix the useEffect in Result page, now translate works with image

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -54,82 +54,44 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-// const callTranslateApi = (wordToTranslate, translateFrom, translateTo) => {
-//   return {
-//     method: 'POST',
-//     url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-//     data: qs.stringify({
-//       q: `${wordToTranslate}`,
-//       source: `${translateFrom}`,
-//       target: `${translateTo}`
-//     }),
-//     headers: {
-//       "content-type": "application/x-www-form-urlencoded",
-//       "accept-encoding": "application/gzip",
-//       "x-rapidapi-host": "google-translate1.p.rapidapi.com",
-//       "x-rapidapi-key": "1acf301addmsh1cf3ba4f25f1c0dp10e434jsn0c3fee614769"
-//     }
-//   }
-// };
-
 const ResultPage = ({ wordToTranslate, translateFrom, translateTo }) => {
   const classes = useStyles();
   const [translateResult, setTranslateResult] = useState('');
   const [imageQuery, setImageQuery] = useState('');
-  const [wordImageUrl, setWordImageUrl] = useState('');
+
+  const translateApiOption = (wordToTranslate, translateFrom, translateTo) => {
+    return ({
+      method: 'POST',
+      url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+      data: qs.stringify({
+        q: `${wordToTranslate}`,
+        source: `${translateFrom}`,
+        target: `${translateTo}`
+      }),
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "accept-encoding": "application/gzip",
+        "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+        "x-rapidapi-key": "1acf301addmsh1cf3ba4f25f1c0dp10e434jsn0c3fee614769"
+      }
+    });
+  };
 
   useEffect(() => {
-    // axios.request({
-    //   method: 'POST',
-    //   url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-    //   data: qs.stringify({
-    //     q: `${wordToTranslate}`,
-    //     source: `${translateFrom}`,
-    //     target: `${translateTo}`
-    //   }),
-    //   headers: {
-    //     "content-type": "application/x-www-form-urlencoded",
-    //     "accept-encoding": "application/gzip",
-    //     "x-rapidapi-host": "google-translate1.p.rapidapi.com",
-    //     "x-rapidapi-key": "b4096686a4msh43536491990dcd7p1cbadcjsnf3dbd6782036"
-    //   }
-    // })
-    // .then(response => setTranslateResult(response.data.data.translations[0].translatedText))
-    // .catch(err => console.log(err));
-
-    // axios.request(callTranslateApi(wordToTranslate, translateFrom, translateTo))
-    // .then(response => setTranslateResult(response.data.data.translations[0].translatedText))
-    // .catch(err => console.log(err));
-
-    setTranslateResult("dog");
-
-  }, [wordToTranslate, translateFrom, translateTo]);
-
-  useEffect(() => {
-    // translateResult && translateFrom === "en"
-    //   ? setImageQuery(wordToTranslate)
-    //   : translateTo === "en"
-    //     ? setImageQuery(translateResult)
-    //     : callTranslateApi(wordToTranslate, translateFrom, "en") && setImageQuery(translateResult);
+    axios.request(translateApiOption(wordToTranslate, translateFrom, translateTo))
+    .then(response => setTranslateResult(response.data.data.translations[0].translatedText))
+    .catch(err => console.log(err));
 
     translateResult && translateFrom === "en"
-    ? setImageQuery(wordToTranslate)
-    : translateTo === "en"
-      ? setImageQuery(translateResult)
-      : setImageQuery(translateResult);
-
-    imageQuery && console.log(imageQuery);
-
-    imageQuery &&
-    fetch(`https://pixabay.com/api/?key=23980639-82f3019418c4f0fe6e840a327&q=${imageQuery}&image_type=illustration`)
-    .then(response => response.json())
-    .then(data => setWordImageUrl(data.hits[0].previewURL))
-    .catch(err => console.log(err));
-  }, [translateResult])
+      ? setImageQuery(wordToTranslate)
+      : translateTo === "en"
+        ? setImageQuery(translateResult)
+        : translateApiOption(wordToTranslate, translateFrom, "en") && setImageQuery(translateResult);
+  }, [wordToTranslate, translateFrom, translateTo, translateResult]);
 
   return (
     <>
-      <Header title={translateResult} bgImageUrl={wordImageUrl} />
+      <Header title={translateResult} imageQuery={imageQuery} />
 
       <div className={classes.bigBox}>
         {/* Column left */}
